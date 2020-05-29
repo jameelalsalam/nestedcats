0f26c79a40f9ff4ac799617580ddb351be31fbab
Jameel Alsalam <alsalam.jameel@epa.gov>
2020-05-29 08:29:24 -0400

    we are making a vctrs vctr...

--- R/partial-order-factor.R
+++ R/partial-order-factor.R
@@ -1,9 +1,21 @@
-#' Low-level constructor for pofct S3 class
+#' Low-level constructor for Partially Ordered Factors `pofct` S3 class
+#'
+#' @param x integer vector to index into labels and partial ordering
+#' @param levels character vector of levels (labels) for pofct values
+#' @param po a data frame representing the partial order
+#'
+#' The partial order is a data frame of edges (e.g., with integer columns `from` and `to`).
+#' Each edge represents a has-subset relation, e.g. `to` <= `from`.
 #'
 #' @import vctrs
+#' @import tibble
 #' @export
-new_pofct <- function(x, po) {
+new_pofct <- function(x = integer(),
+                      labels = character(),
+                      po = tibble()) {
   vec_assert(x, integer())
+  stopifnot(is.character(labels))
+  stopifnot(inherits(tibble(), "data.frame"))
 }
 
 #' Format S3 method
@@ -17,4 +29,28 @@ format.pofct <- function(x) {
 #'
 validate_pofct <- function(x) {
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Math and arithmetic -----------------------------------------------------
+
+#' @export
+vec_math.pofct <- function(.fn, .x, ...) {
+  stop_unsupported(.x, .fn)
+}
+
+#' @export
+vec_arith.pofct <- function(op, x, y, ...) {
+  stop_unsupported(x, op)
 }
